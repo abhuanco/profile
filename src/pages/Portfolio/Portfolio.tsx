d8a7c3369b9568fb2c6c622b7f27bd85ff22232a
Rene Huanco Choque <rene.huanco.choque@gmail.com>
2023-06-16 14:33:57 -0400

    [react-v1.0][ADD]: Add portfolio design

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -5,7 +5,49 @@ export class Portfolio extends Component {
     render() {
         return (
             <>
-                <h2>Portfolio services</h2>
+                <div className="row g-2">
+                    <div className=" col-xl-4 col-lg-4 col-md-4 col-sm-6">
+                        <div className="card">
+                            <img src="https://picsum.photos/id/1/500/300" className="card-img-top" alt="Project"/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">
+                                    Dsecription website example what your offer
+                                </p>
+                                <div>
+                                    <code>PHP</code>, <code>Prestashop</code>, <code>HTML</code>, <code>Javascript</code>
+                                </div>
+                                <a href="https://www.google.com" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className=" col-xl-4 col-lg-4 col-md-4 col-sm-6">
+                        <div className="card">
+                            <img src="https://picsum.photos/id/1/500/300" className="card-img-top" alt="Project"/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">
+                                    Some quick example text to build on the card title and make
+                                    up the bulk of the card's content.
+                                </p>
+                                <a href="https://www.google.com" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className=" col-xl-4 col-lg-4 col-md-4 col-sm-6">
+                        <div className="card">
+                            <img src="https://picsum.photos/id/1/500/300" className="card-img-top" alt="Project"/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">
+                                    Some quick example text to build on the card title and make
+                                    up the bulk of the card's content.
+                                </p>
+                                <a href="https://www.google.com" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </>
         )
     }
---
+++ src/utils/Design.ts
@@ -0,0 +1,7 @@
+export const design = () => {
+    return {
+        grids: {
+            row: 'row'
+        }
+    }
+};
