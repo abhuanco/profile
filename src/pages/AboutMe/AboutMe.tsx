bc038f1c403f4c052b6d7760b6544fff48e47a3b
Rene Huanco Choque <rene.huanco.choque@gmail.com>
2023-06-28 14:54:09 -0400

    [react-v1.0][UPD]: Work in about me text

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -1,18 +1,15 @@
-import {Component} from "react";
 import './AboutMe.scss'
-import Translate from "../../utils/translate";
+import {useTranslation} from "react-i18next";
 
-export class AboutMe extends Component {
-
-    render = () => (
-        <>
-            <h1>{Translate('menu.about-me')}</h1>
-            <h2>{Translate('titles.senior')}</h2>
-            <p>{Translate('pages.about-me.about_me_01')}</p>
-            <p>{Translate('pages.about-me.about_me_02')}</p>
-            <p>{Translate('pages.about-me.about_me_03')}</p>
-            <p>{Translate('pages.about-me.about_me_04')}</p>
-            <p>{Translate('pages.about-me.about_me_05')}</p>
-        </>
-    );
-}
+export const AboutMe = () =>  {
+    const {t} = useTranslation("translations");
+    return (<>
+        <h1>{t('menu.about-me')}</h1>
+        <h2>{t('titles.senior')}</h2>
+        <p>{t('pages.about-me.about_me_01')}</p>
+        <p>{t('pages.about-me.about_me_02')}</p>
+        <p>{t('pages.about-me.about_me_03')}</p>
+        <p>{t('pages.about-me.about_me_04')}</p>
+        <p>{t('pages.about-me.about_me_05')}</p>
+    </>)
+};
--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -2,6 +2,6 @@
 import {useTranslation} from "react-i18next";
 
 export default function Translate(key: string) {
-    const [t,] = useTranslation("translations");
+    const {t} = useTranslation("translations");
     return t(`${key}`);
 }
