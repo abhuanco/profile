716508ebe7b69de0ca9bd58db274a70a5a589285
Rene Huanco Choque <rene.huanco.choque@gmail.com>
2023-06-28 14:44:36 -0400

    [react-v1.0][UPD]: Work in about me text

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -1,10 +1,18 @@
 import {Component} from "react";
 import './AboutMe.scss'
+import Translate from "../../utils/translate";
 
 export class AboutMe extends Component {
+
     render = () => (
         <>
-            <h2>About me</h2>
+            <h1>{Translate('menu.about-me')}</h1>
+            <h2>{Translate('titles.senior')}</h2>
+            <p>{Translate('pages.about-me.about_me_01')}</p>
+            <p>{Translate('pages.about-me.about_me_02')}</p>
+            <p>{Translate('pages.about-me.about_me_03')}</p>
+            <p>{Translate('pages.about-me.about_me_04')}</p>
+            <p>{Translate('pages.about-me.about_me_05')}</p>
         </>
     );
 }
---
+++ src/utils/translate.ts
@@ -0,0 +1,7 @@
+
+import {useTranslation} from "react-i18next";
+
+export default function Translate(key: string) {
+    const [t,] = useTranslation("translations");
+    return t(`${key}`);
+}
