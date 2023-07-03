4402b307ebadf3bde5f52b365d871fb0a7e36824
Rene Huanco Choque <rene.huanco.choque@gmail.com>
2023-07-03 14:35:03 -0400

    [react-v1.0][UPD]: Add changes on skills

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -6,12 +6,20 @@ export class Skills extends Component {
         return (
             <>
                 <h1>My skills list:</h1>
-                <ul className="list-group-horizontal">
-                    <li>PHP</li>
-                    <li>NodeJS</li>
-                    <li>React</li>
-                    <li>Angular</li>
-                </ul>
+                <div className="row d-flex justify-content-center">
+                    <div className="col-lg-8 col-md-8 col-sm-12">
+                        <div className="row">
+                            <div className="col-lg-4 col-md-4 col-sm-6">
+                                <div className="bss-card-skill">
+                                    <img src="https://www.php.net/images/logos/new-php-logo.svg" alt="PHP"/>
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-4 col-sm-6">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </>
         )
     }
